c963ac28af7a2eb4c59cbb0d5199ae02a6d4f1f4
obscuremax <maxxam@yandex.ru>
2024-11-03 21:41:44 +0300

    jdisnciapu

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,42 @@
 <script setup>
-const title = 'ЦЦЦЦ'
+const cards = [
+  {
+    title: 'Метеорология',
+    image: 'meteorology.jpg'
+  },{
+    title: 'Радиолокация',
+    image: 'meteoradar.jpg'
+  },{
+    title: 'Спутниковые снимки',
+    image: 'sputnik.jpg'
+  },{
+    title: 'Гидрология',
+    image: 'gidrology.jpg'
+  },{
+    title: 'Радиационный мониторинг',
+    image: 'radiation.jpg'
+  },{
+    title: 'Рисамос',
+    image: 'vibrosi.jpg'
+  },
+]
+const bgImage = (image) => `background-image: url("./${image}")`
 </script>
 
 <template>
   <div>
-    <h1>{{title}}</h1>
+    <section class="container-cards">
+
+          <div class="card" v-for="card in cards">
+            <div class="cover" :style="bgImage(card.image)">
+              <h1>{{card.title}}</h1>
+<!--              <span class="price">$35</span>-->
+              <div class="card-back">
+                <a href="#">Перейти</a>
+              </div>
+            </div>
+          </div>
+
+    </section>
   </div>
 </template>
